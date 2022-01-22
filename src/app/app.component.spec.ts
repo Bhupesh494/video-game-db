4808e88c1ea557f5a84b841790e9af952c07bcaa
Bhupesh Rajpurohit <bhupeshpurohit494@gmail.com>
2022-01-22 17:24:48 +0530

    created home component, structured the component and created route for it

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -5,12 +5,8 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterTestingModule],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
@@ -23,13 +19,15 @@ describe('AppComponent', () => {
   it(`should have as title 'ng-video-game-db'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ng-video-game-db');
+    expect(app['title']).toEqual('ng-video-game-db');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('ng-video-game-db app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain(
+      'ng-video-game-db app is running!'
+    );
   });
 });
